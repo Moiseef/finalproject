593d38697d2159fff62a3ac1425eb7c8dd0add0e
Igor Moiseev <igormoiseev@Igors-MacBook-Air.local>
2023-04-07 14:26:47 +0200

    with path to view realty

---
+++ realty/static/realty/assets/myjs.js
@@ -0,0 +1,41 @@
+//header
+function myHeaderfix() {
+    let scroll = window.scrollY;
+    let scrollStart = 100;
+    let myheader = document.querySelector('#myheader');
+    let mybody = document.querySelector('body');
+    const changeHeader = () =>  myheader.classList.add('fixhead');
+    const changeBody = () => mybody.classList.add('mtbody');
+    const removeHeader = () =>  myheader.classList.remove('fixhead');
+    const removeBody = () => mybody.classList.remove('mtbody');
+
+    window.addEventListener('scroll' , () =>{
+        scroll = window.scrollY;
+        if (scroll >= scrollStart) { changeHeader(), changeBody() }
+        else{ removeHeader(), removeBody() }
+    })
+}
+myHeaderfix();
+//realry string
+function myCut() {
+let myStr = document.querySelector('#mycuttext');
+myStr.slice(5);
+}
+myCut();
+
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
